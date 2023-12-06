2d6ea0931cef00bd559bc88396c22aea7dc64f2e
unknown <thpthv.nguyenphuduykhang@gmail.com>
2023-12-06 15:08:38 +0200

    setting mongoDB

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ let bodyParser = require("body-parser");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 
+dotenv.config();
+//connect database
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Successfully connected to MongoDB!");
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB: ", error);
+  });
+
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(cors());
 app.use(morgan("common"));
