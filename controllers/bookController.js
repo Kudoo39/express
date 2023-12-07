46d748134aefe571621f5a49210049727a45df0f
unknown <thpthv.nguyenphuduykhang@gmail.com>
2023-12-07 03:48:24 +0200

    bookController add a book

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -2,7 +2,14 @@ const { Author, Book } = require("../model/model");
 
 const bookController = {
   //add a book
-  addABook: async (req, res) => {},
+  addABook: async (req, res) => {
+    try {
+      const newBook = new Book(req.body);
+      const savedBook = await newBook.save();
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
 };
 
 module.exports = bookController;
