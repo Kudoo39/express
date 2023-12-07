ce5d589b4b85d0a301b6bbd196f1a2e1b2b13a5f
unknown <thpthv.nguyenphuduykhang@gmail.com>
2023-12-07 16:19:50 +0200

    adding book and get all books

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -6,6 +6,21 @@ const bookController = {
     try {
       const newBook = new Book(req.body);
       const savedBook = await newBook.save();
+      if (req.body.author) {
+        // const author = Author.find({ _id: req.body.author });
+        const author = Author.findById(req.body.author);
+        await author.updateOne({ $push: { books: savedBook._id } });
+      }
+      res.status(200).json(savedBook);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
+  //get all books
+  getAllBooks: async (req, res) => {
+    try {
+      const allBooks = await Book.find();
+      res.status(500).json(allBooks);
     } catch (error) {
       res.status(500).json(error);
     }
--- routes/book.js
+++ routes/book.js
@@ -5,4 +5,7 @@ const router = require("express").Router();
 //add a book
 router.post("/", bookController.addABook);
 
+//get all books
+router.get("/", bookController.getAllBooks);
+
 module.exports = router;
